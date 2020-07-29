4dcf6372c790ca17715b7d020cb21bbf2638ff18
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-07-29 17:06:02 -0300

    Correcao FormField

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,32 +1,24 @@
 import React from 'react';
 
 const FormField = ({label, type, ...rest}) => {
-  if (type === "textarea"){
-    return(
-      <div>
-      <label>
-        {label}
-
+  return(
+    <div>
+    <label>
+      {label}
+      {type === "textarea" ?
         <textarea
           type = {type}
           {...rest}
         />
-      </label>
-    </div>
-    );
-  }
-  
-  return(
-    <div>
-      <label>
-        {label}
-
+        :
         <input 
           type = {type}
           {...rest}
         />
-      </label>
-    </div>
+      } 
+      
+    </label>
+  </div>
   );
 }
 
